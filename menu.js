1e00f5fb5d24a0f92dd16819238822ae4044201c
rashonwill <75400387+rashonwill@users.noreply.github.com>
2021-10-20 10:49:22 -0500

    Update menu.js

--- menu.js
+++ menu.js
@@ -107,6 +107,7 @@ $('#tacos').on('click', function(){
   $('.btn').text('Added to Cart!');	
   $('.count').empty();
 let updateCart = count + 1;
+    console.log(updateCart)
 let totalBag = localStorage.setItem('cartItems', updateCart);
 let newCart = localStorage.getItem('cartItems');           
            
