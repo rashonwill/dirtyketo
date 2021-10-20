a70ff580a4d7167884104d5d1654d3e1a70baf9d
rashonwill <75400387+rashonwill@users.noreply.github.com>
2021-10-20 07:59:52 -0500

    Update menu.js

---
+++ menu.js
@@ -0,0 +1,515 @@
+function renderAppetizers(){ 
+  $('.main').empty();
+let apps =  (`<div class="title"> Appetizers</div>
+  <div class="food-options">
+    
+  <div class="card" style="width: 18rem;">
+  <img class="card-img-top" src="https://www.dinneratthezoo.com/wp-content/uploads/2018/01/taco-cups-6-500x500.jpg" alt="Card image cap">
+  <div class="card-body">
+    <p class="card-text">Taco Bites</p>
+    <a href="#" class="btn">Order</a>
+  </div>
+</div>  
+  <div class="card" style="width: 18rem;">
+  <img class="card-img-top" src="https://www.patrickmaese.com/wp-content/uploads/2019/03/Chili-Cheese-Dogs-blog-1-500x500.jpg" alt="Card image cap">
+  <div class="card-body">
+    <p class="card-text">Chilli Cheese Hot Dogs</p>
+    <a href="#" class="btn">Order</a>
+  </div>
+</div>  
+    
+  <div class="card" style="width: 18rem;">
+  <img class="card-img-top" src="https://theketoqueens.com/wp-content/uploads/2018/04/Low-Carb-Keto-Tortilla-Chips-Recipe-500x375.jpg" alt="Card image cap">
+  <div class="card-body">
+    <p class="card-text">Guacamole & Chips</p>
+    <a href="#" class="btn">Order</a>
+  </div>
+</div>  
+  <div class="card" style="width: 18rem;">
+  <img class="card-img-top" src="https://www.foodnetwork.com/content/dam/images/food/video/9/94/947/9471/9471366.jpg" alt="Card image cap">
+  <div class="card-body">
+    <p class="card-text">Bacon Cheese Burger Bites</p>
+    <a href="#" class="btn">Order</a>
+  </div>
+</div>     
+       
+  </div>`);
+  $('.main').append(apps);
+  return apps;
+};
+
+renderAppetizers();
+
+
+
+function renderBurgers(){
+   $('.main').empty();
+ let burgers =  (`<div class="title"> Burgers</div>
+  <div class="food-options">
+    
+  <div class="card" style="width: 18rem;">
+  <img class="card-img-top" src="https://kaseytrenum.com/wp-content/uploads/2020/05/keto-stuffed-burger.jpg" alt="Card image cap">
+  <div class="card-body">
+    <p class="card-text">The Plain Jane</p>
+    <a href="#" class="btn">Order</a>
+  </div>
+</div>  
+
+  <div class="card" style="width: 18rem;">
+  <img class="card-img-top" src="https://www.ketodietyum.com/wp-content/uploads/2020/06/Keto-Bunless-Burger-Recipes.jpg" alt="Card image cap">
+  <div class="card-body">
+    <p class="card-text">The Big Boy</p>
+    <a href="#" class="btn">Order</a>
+  </div>
+</div>  
+    
+  <div class="card" style="width: 18rem;">
+  <img class="card-img-top" src="https://pbs.twimg.com/media/DVyp25YWsAA30ik.jpg">
+  <div class="card-body">
+    <p class="card-text">The All You Need</p>
+    <a href="#" class="btn">Order</a>
+  </div>
+</div>  
+
+  <div class="card" style="width: 18rem;">
+  <img class="card-img-top" src="https://www.dontgobaconmyheart.co.uk/wp-content/uploads/2020/03/cheeseburger-wraps-500x500.jpg" alt="Card image cap">
+  <div class="card-body">
+    <p class="card-text">Cheese Burger Wraps</p>
+    <a href="#" class="btn">Order</a>
+  </div>
+</div>     
+       
+  </div>`);
+    $('.main').append(burgers);
+  return burgers;
+  
+};
+
+
+function renderSteakAndShrimp(){
+   $('.main').empty();
+  let steakShrimp =  (`<div class="title"> Steak And Shrimp</div>
+  <div class="food-options">
+    
+  <div class="card" style="width: 18rem;">
+  <img class="card-img-top" src="https://www.aymag.com/wp-content/uploads/2020/05/Garlic-Butter-Steak-Shrimp-Recipe-IMAGE-1.jpg" alt="Card image cap">
+  <div class="card-body">
+    <p class="card-text">Steak and Garlic Shrimp</p>
+    <a href="#" class="btn">Order</a>
+  </div>
+</div>  
+
+  <div class="card" style="width: 18rem;">
+  <img class="card-img-top" src="https://www.noshtastic.com/wp-content/uploads/2018/08/low-carb-beef-and-broccoli-sq-1-of-1.jpg" alt="Card image cap">
+  <div class="card-body">
+    <p class="card-text">Steak and Broccoli Skillet</p>
+    <a href="#" class="btn">Order</a>
+  </div>
+</div>  
+    
+  <div class="card" style="width: 18rem;">
+  <img class="card-img-top" src="https://www.justataste.com/wp-content/uploads/2016/06/skinny-shrimp-scampi-photo.jpg" alt="Card image cap">
+  <div class="card-body">
+    <p class="card-text">Shrimp Scampi</p>
+    <a href="#" class="btn">Order</a>
+  </div>
+</div>  
+
+  <div class="card" style="width: 18rem;">
+  <img class="card-img-top" src="https://dadwithapan.com/wp-content/uploads/2017/08/Steak-Shrimp-Paleo-Bowl-1-656x875.jpg" alt="Card image cap">
+  <div class="card-body">
+    <p class="card-text">Shrimp and Steak Bowl with Veggies</p>
+    <a href="#" class="btn">Order</a>
+  </div>
+</div>     
+       
+  </div>`);
+      $('.main').append(steakShrimp);
+  return steakShrimp;
+  
+};
+
+
+function renderWings(){
+   $('.main').empty();
+  let wings =  (`<div class="title"> Wings</div>
+  <div class="food-options">
+    
+  <div class="card" style="width: 18rem;">
+  <img class="card-img-top" src="https://www.allchickenrecipes.com/wp-content/uploads/2019/12/keto-chicken-wings.jpg" alt="Card image cap">
+  <div class="card-body">
+    <p class="card-text">Buffalo Wings</p>
+    <a href="#" class="btn">Order</a>
+  </div>
+</div>  
+
+  <div class="card" style="width: 18rem;">
+  <img class="card-img-top" src="https://c9u8e9q4.rocketcdn.me/wp-content/uploads/2021/06/Air-Fryer-BBQ-Chicken-Wings-2.jpg-V2-vertical-1200-800x1200.jpg" alt="Card image cap">
+  <div class="card-body">
+    <p class="card-text">BBQ Wings</p>
+    <a href="#" class="btn">Order</a>
+  </div>
+</div>  
+    
+  <div class="card" style="width: 18rem;">
+  <img class="card-img-top" src="https://homemadehooplah.com/wp-content/uploads/2019/05/lemon-pepper-chicken-wings-1.jpg" alt="Card image cap">
+  <div class="card-body">
+    <p class="card-text">Lemon Pepper Wings</p>
+    <a href="#" class="btn">Order</a>
+  </div>
+</div>  
+
+  <div class="card" style="width: 18rem;">
+  <img class="card-img-top" src="https://theketocookbook.com/wp-content/uploads/2018/05/wings-1040x1300.jpg" alt="Card image cap">
+  <div class="card-body">
+    <p class="card-text">Garlic Parmesan</p>
+    <a href="#" class="btn">Order</a>
+  </div>
+</div>     
+       
+  </div>`);
+      $('.main').append(wings);
+  return wings;
+  
+};
+
+function renderSalads(){
+   $('.main').empty();
+  let salads =  (`<div class="title"> Salads</div>
+  <div class="food-options">
+    
+  <div class="card" style="width: 18rem;">
+  <img class="card-img-top" src="https://www.eatwell101.com/wp-content/uploads/2018/01/shrimp-avocado-tomato-salad.jpg" alt="Card image cap">
+  <div class="card-body">
+    <p class="card-text">Shrimp Avacoda and Tomato Salad</p>
+    <a href="#" class="btn">Order</a>
+  </div>
+</div>  
+
+  <div class="card" style="width: 18rem;">
+  <img class="card-img-top" src="https://simply-delicious-food.com/wp-content/uploads/2019/05/blt-chicken-salad-3.jpg" alt="Card image cap">
+  <div class="card-body">
+    <p class="card-text">BLT Chicken Salad</p>
+    <a href="#" class="btn">Order</a>
+  </div>
+</div>  
+    
+  <div class="card" style="width: 18rem;">
+  <img class="card-img-top" src="https://www.saltandlavender.com/wp-content/uploads/2018/03/low-carb-taco-salad-recipe-1.jpg" alt="Card image cap">
+  <div class="card-body">
+    <p class="card-text">Taco Salad</p>
+    <a href="#" class="btn">Order</a>
+  </div>
+</div>  
+
+  <div class="card" style="width: 18rem;">
+  <img class="card-img-top" src="" alt="Card image cap">
+  <div class="card-body">
+    <p class="card-text">Avacado Kale Salad</p>
+    <a href="#" class="btn">Order</a>
+  </div>
+</div>     
+       
+  </div>`);
+      $('.main').append(salads);
+  return salads;
+  
+};
+
+
+function renderSides(){
+   $('.main').empty();
+  let sides =  (`<div class="title"> Sides</div>
+  <div class="food-options">
+    
+  <div class="card" style="width: 18rem;">
+  <img class="card-img-top" src="https://hip2keto.com/wp-content/uploads/sites/3/2019/10/Untitled-13.jpg?resize=1024%2C538&strip=all" alt="Card image cap">
+  <div class="card-body">
+    <p class="card-text">Jicama Fries</p>
+    <a href="#" class="btn">Order</a>
+  </div>
+</div>  
+  <div class="card" style="width: 18rem;">
+  <img class="card-img-top" src="https://i.pinimg.com/originals/74/d4/dd/74d4dde19f0934d65c60da8a029f62a3.jpg" alt="Card image cap">
+  <div class="card-body">
+    <p class="card-text">Loaded Cauliflower Mashed</p>
+    <a href="#" class="btn">Order</a>
+  </div>
+</div>  
+    
+  <div class="card" style="width: 18rem;">
+  <img class="card-img-top" src="https://img.buzzfeed.com/thumbnailer-prod-us-east-1/video-api/assets/224521.jpg" alt="Card image cap">
+  <div class="card-body">
+    <p class="card-text">Cauliflower Mac n Cheese</p>
+    <a href="#" class="btn">Order</a>
+  </div>
+</div>  
+
+  <div class="card" style="width: 18rem;">
+  <img class="card-img-top" src="https://static.wixstatic.com/media/fed9c0_af0731135594433d879ec1e744bd49ea~mv2.jpg/v1/fill/w_1000,h_725,al_c,q_90,usm_0.66_1.00_0.01/fed9c0_af0731135594433d879ec1e744bd49ea~mv2.jpg" alt="Card image cap">
+  <div class="card-body">
+    <p class="card-text">Bacon Greenbeans</p>
+    <a href="#" class="btn">Order</a>
+  </div>
+</div>     
+       
+  </div>`);
+      $('.main').append(sides);
+  return sides;
+  
+};
+
+function renderChacuterie(){
+   $('.main').empty();
+  let platters =  (`<div class="title">Chacuterie</div>
+  <div class="food-options">
+    
+  <div class="card" style="width: 18rem;">
+  <img class="card-img-top" src="https://www.lowcarbnomad.com/wp-content/uploads/2020/09/Keto-Breakfast-Charcuterie-Board-21-500x500.jpg" alt="Card image cap">
+  <div class="card-body">
+    <p class="card-text">Breakfast Charcuterie</p>
+    <a href="#" class="btn">Order</a>
+  </div>
+</div>  
+
+  <div class="card" style="width: 18rem;">
+  <img class="card-img-top" src="https://thebigmansworld.com/wp-content/uploads/2021/07/keto-charcuterie-board-4-500x500.jpeg" alt="Card image cap">
+  <div class="card-body">
+    <p class="card-text">The Fullfillment</p>
+    <a href="#" class="btn">Order</a>
+  </div>
+</div>  
+    
+  <div class="card" style="width: 18rem;">
+  <img class="card-img-top" src="https://www.lowcarbingasian.com/wp-content/uploads/2019/05/Keto-Charcuterie-Board-LowCarbingAsian-Cover.jpg" alt="Card image cap">
+  <div class="card-body">
+    <p class="card-text">K.I.S.S</p>
+    <a href="#" class="btn">Order</a>
+  </div>
+</div>  
+
+  <div class="card" style="width: 18rem;">
+  <img class="card-img-top" src="https://media1.popsugar-assets.com/files/thumbor/gfEQPn5K5DZkzH4Mg7fDJ47o1Ns/fit-in/1024x1024/filters:format_auto-!!-:strip_icc-!!-/2020/12/17/057/n/1922729/0c0499a85fdbf632d9de26.45494820_/i/fruit-charcuterie-board-ideas.jpg" alt="Card image cap">
+  <div class="card-body">
+    <p class="card-text">The Very Bery</p>
+    <a href="#" class="btn">Order</a>
+  </div>
+</div>     
+       
+  </div>`);
+      $('.main').append(platters);
+  return platters;
+};
+
+//NEXT
+
+function renderLunches(){
+   $('.main').empty();
+  let lunches =  (`<div class="title">Daily Lunches</div>
+  <div class="food-options">
+    
+  <div class="card" style="width: 18rem;">
+  <img class="card-img-top" src="https://mamabearscookbook.com/wp-content/uploads/2019/10/low-carb-beef-stew-feature.jpg" alt="Card image cap">
+  <div class="card-body">
+    <p class="card-text">Beef Stew and Keto Rolls</p>
+    <a href="#" class="btn">Order</a>
+  </div>
+</div>  
+
+  <div class="card" style="width: 18rem;">
+  <img class="card-img-top" src="https://cdn.greatlifepublishing.net/wp-content/uploads/sites/2/2017/02/22121653/Kielbasa-Fried-Cabbage-Horizontal-5.jpg" alt="Card image cap">
+  <div class="card-body">
+    <p class="card-text">Cabage and Sausage Bowl</p>
+    <a href="#" class="btn">Order</a>
+  </div>
+</div>  
+    
+  <div class="card" style="width: 18rem;">
+  <img class="card-img-top" src="https://ketopots.com/wp-content/uploads/2020/02/5a-keto-spaghetti.jpg" alt="Card image cap">
+  <div class="card-body">
+    <p class="card-text">Meatballs with Egg Noodles</p>
+    <a href="#" class="btn">Order</a>
+  </div>
+</div>  
+  <div class="card" style="width: 18rem;">
+  <img class="card-img-top" src="https://i2.wp.com/homemadeinterest.com/wp-content/uploads/2018/01/Low-Carb-Meatloaf_featured-FB.jpg" alt="Card image cap">
+  <div class="card-body">
+    <p class="card-text">Meatloaf</p>
+    <a href="#" class="btn">Order</a>
+  </div>
+</div>     
+       
+  </div>`);
+      $('.main').append(lunches);
+  return lunches;
+  
+};
+
+function renderKids(){
+   $('.main').empty();
+  let kids=  (`<div class="title">Kids Menu</div>
+  <div class="food-options">
+    
+  <div class="card" style="width: 18rem;">
+  <img class="card-img-top" src="https://www.thedietchefs.com/wp-content/uploads/2019/04/Keto-Pizza-in-10-MINUTES-This-the-is-BEST-Pizza-for-keto-SO-MUCH-BETTER-than-Fat-head-Crust-Pizza.png" alt="Card image cap">
+  <div class="card-body">
+    <p class="card-text">Pizza Slice</p>
+    <a href="#" class="btn">Order</a>
+  </div>
+</div>  
+
+  <div class="card" style="width: 18rem;">
+  <img class="card-img-top" src="https://webknox.com/recipeImages/983213-556x370.jpg" alt="Card image cap">
+  <div class="card-body">
+    <p class="card-text">Chicken Nuggets</p>
+    <a href="#" class="btn">Order</a>
+  </div>
+</div>  
+    
+  <div class="card" style="width: 18rem;">
+  <img class="card-img-top" src="https://www.tasteaholics.com/wp-content/uploads/2019/01/Crispy-Baked-Fish-Sticks-feature.jpg" alt="Card image cap">
+  <div class="card-body">
+    <p class="card-text">Fish Sticks</p>
+    <a href="#" class="btn">Order</a>
+  </div>
+</div>  
+  <div class="card" style="width: 18rem;">
+  <img class="card-img-top" src="https://www.officiallyglutenfree.com/wp-content/uploads/2020/03/Low-Carb-Chicken-Club-Sandwich-17.jpg" alt="Card image cap">
+  <div class="card-body">
+    <p class="card-text">Club Sandwhiches</p>
+    <a href="#" class="btn">Order</a>
+  </div>
+</div>     
+       
+  </div>`);
+      $('.main').append(kids);
+  return kids;
+};
+
+function renderDesserts(){
+   $('.main').empty();
+  let desserts =  (`<div class="title">Desserts</div>
+  <div class="food-options">
+    
+  <div class="card" style="width: 18rem;">
+  <img class="card-img-top" src="https://www.fittoservegroup.com/wp-content/uploads/2020/02/keto-blueberry-cheesecake-on-white-plate-2-2-720x720.jpg" alt="Card image cap">
+  <div class="card-body">
+    <p class="card-text">Blueberry Cheesecake</p>
+    <a href="#" class="btn">Order</a>
+  </div>
+</div>  
+
+  <div class="card" style="width: 18rem;">
+  <img class="card-img-top" src="https://www.thespruceeats.com/thmb/hSpbo9r3zma8clXXd-mXeqzu7l4=/1332x1332/smart/filters:no_upscale()/keto-chocolate-cookies-4769153-hero-01-a4287551d0eb4d86a4bc3bb0afb6c6d8.jpg" alt="Card image cap">
+  <div class="card-body">
+    <p class="card-text">Double Chocolate Chip Cookes</p>
+    <a href="#" class="btn">Order</a>
+  </div>
+</div>  
+    
+  <div class="card" style="width: 18rem;">
+  <img class="card-img-top" src="https://www.ketoconnect.net/wp-content/uploads/2020/12/close-up-pile-of-cookies-with-sea-salt.jpg" alt="Card image cap">
+  <div class="card-body">
+    <p class="card-text">Classic Chocolate Chip</p>
+    <a href="#" class="btn">Order</a>
+  </div>
+</div>  
+  <div class="card" style="width: 18rem;">
+  <img class="card-img-top" src="https://joyfilledeats.com/wp-content/uploads/2017/10/keto-vanilla-ice-cream-1-500x375.jpg" alt="Card image cap">
+  <div class="card-body">
+    <p class="card-text">Brownie and Vanilla Ice Cream</p>
+    <a href="#" class="btn">Order</a>
+  </div>
+</div>     
+       
+  </div>`) 
+    $('.main').append(desserts);
+  return desserts;
+  
+};
+
+//TO DO 
+function renderBeverages(){
+   $('.main').empty();
+  let drinks =  (`<div class="title"> Beverages</div>
+  <div class="food-options">
+    
+  <div class="card" style="width: 18rem;">
+  <img class="card-img-top" src="https://www.dinneratthezoo.com/wp-content/uploads/2018/01/taco-cups-6-500x500.jpg" alt="Card image cap">
+  <div class="card-body">
+    <p class="card-text">Taco Bites</p>
+    <a href="#" class="btn">Order</a>
+  </div>
+</div>  
+  <div class="card" style="width: 18rem;">
+  <img class="card-img-top" src="https://www.patrickmaese.com/wp-content/uploads/2019/03/Chili-Cheese-Dogs-blog-1-500x500.jpg" alt="Card image cap">
+  <div class="card-body">
+    <p class="card-text">Chilli Cheese Hot Dogs</p>
+    <a href="#" class="btn">Order</a>
+  </div>
+</div>  
+    
+  <div class="card" style="width: 18rem;">
+  <img class="card-img-top" src="https://theketoqueens.com/wp-content/uploads/2018/04/Low-Carb-Keto-Tortilla-Chips-Recipe-500x375.jpg" alt="Card image cap">
+  <div class="card-body">
+    <p class="card-text">Guacamole & Chips</p>
+    <a href="#" class="btn">Order</a>
+  </div>
+</div>  
+  <div class="card" style="width: 18rem;">
+  <img class="card-img-top" src="https://www.foodnetwork.com/content/dam/images/food/video/9/94/947/9471/9471366.jpg" alt="Card image cap">
+  <div class="card-body">
+    <p class="card-text">Bacon Cheese Burger Bites</p>
+    <a href="#" class="btn">Order</a>
+  </div>
+</div>     
+       
+  </div>`);
+      $('.main').append(drinks);
+  return drinks;
+  
+};
+
+//TO DO
+function renderDeals(){
+   $('.main').empty();
+  let deals =  (`<div class="title"> Meal Deals</div>
+  <div class="food-options">
+    
+  <div class="card" style="width: 18rem;">
+  <img class="card-img-top" src="https://www.dinneratthezoo.com/wp-content/uploads/2018/01/taco-cups-6-500x500.jpg" alt="Card image cap">
+  <div class="card-body">
+    <p class="card-text">Taco Bites</p>
+    <a href="#" class="btn">Order</a>
+  </div>
+</div>  
+  <div class="card" style="width: 18rem;">
+  <img class="card-img-top" src="https://www.patrickmaese.com/wp-content/uploads/2019/03/Chili-Cheese-Dogs-blog-1-500x500.jpg" alt="Card image cap">
+  <div class="card-body">
+    <p class="card-text">Chilli Cheese Hot Dogs</p>
+    <a href="#" class="btn">Order</a>
+  </div>
+</div>  
+    
+  <div class="card" style="width: 18rem;">
+  <img class="card-img-top" src="https://theketoqueens.com/wp-content/uploads/2018/04/Low-Carb-Keto-Tortilla-Chips-Recipe-500x375.jpg" alt="Card image cap">
+  <div class="card-body">
+    <p class="card-text">Guacamole & Chips</p>
+    <a href="#" class="btn">Order</a>
+  </div>
+</div>  
+  <div class="card" style="width: 18rem;">
+  <img class="card-img-top" src="https://www.foodnetwork.com/content/dam/images/food/video/9/94/947/9471/9471366.jpg" alt="Card image cap">
+  <div class="card-body">
+    <p class="card-text">Bacon Cheese Burger Bites</p>
+    <a href="#" class="btn">Order</a>
+  </div>
+</div>     
+       
+  </div>`);
+      $('.main').append(deals);
+  return deals;
+  
+};
+
+
