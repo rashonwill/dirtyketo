20893a6963b07ff0b106914ef84ebd84946de7be
rashonwill <75400387+rashonwill@users.noreply.github.com>
2022-02-09 15:11:32 -0600

    Update index.js

--- index.js
+++ index.js
@@ -11,6 +11,11 @@ $('.count').text(spending);
 
 
 (function(){
+  let spending = parseInt(localStorage.getItem('cartItems'))
+  if (spending > 1){
+  $('.modal-body').text('Total Due: $32.76') 
+  }else if(spending === 0){
   let cartTot = localStorage.setItem('cartTotal', cartTotal)
   $('.modal-body').text('Your cart is empty') 
+  }
 })();
