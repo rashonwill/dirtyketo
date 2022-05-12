9b5c9ccdc1a002ce7eb30251adb8e826901f852a
rashonwill <75400387+rashonwill@users.noreply.github.com>
2022-05-12 16:15:45 -0500

    Update options.js

--- options.js
+++ options.js
@@ -11,25 +11,31 @@ $('.count').text(spending);
 
 
 // (function(){
+// 	let items =[]
 //   let spending = parseInt(localStorage.getItem('cartCount'))
 //   if (spending >= 1){
 //     let cartItems = JSON.parse(localStorage.getItem('MyItems'))
-//     let food = $(`
+//     items.push(cartItems)
+// 	  console.log(items)
+//     cartItems.forEach(async function(items){
+//         let food = $(`
 //        <tr class="myPurchase">
-//       <td id="image"><img class="pic" src="${cartItems.image}"></td>
-//       <td>${cartItems.name}</td>
+//       <td id="image"><img class="pic" src="${items.image}"></td>
+//       <td>${items.name}</td>
 //       <td></td>
 //       <td class="increase"><input type="text" value="1" class="item-qty"></td>
-//       <td class="item-price">${cartItems.price}</td>
+//       <td class="item-price">${items.price}</td>
 //       <td><i class="fa fa-times-circle-o" aria-hidden="true"></i></td>
 //     </tr> 
 // `).data('cartItems', cartItems)
-//   $('.modal-body').append(cartItems) 
+//   $('#inthebag tbody').append(food) 
+//    })
+
 //   }else if(spending == 0){
 //   let cartTot = localStorage.setItem('cartTotal', cartTotal)
 //   $('.empty-cart').text('Your cart is empty') 
 //   }
-// })();
+// })()
 
 
 
