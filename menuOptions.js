5af359450d6cbfc7a710127795453f6b8639c6de
rashonwill <75400387+rashonwill@users.noreply.github.com>
2022-05-12 11:41:40 -0500

    Create menuOptions.js

---
+++ menuOptions.js
@@ -0,0 +1,33 @@
+const menuCategories = [
+  {id: 1, image: "https://www.dinneratthezoo.com/wp-content/uploads/2018/01/taco-cups-6-500x500.jpg" , title: "Appetizers"},
+  {id: 2, image: "https://www.ketodietyum.com/wp-content/uploads/2020/06/Keto-Bunless-Burger-Recipes.jpg" , title: "Burgers"},
+  {id: 3, image: "https://www.ketodietyum.com/wp-content/uploads/2020/06/Keto-Bunless-Burger-Recipes.jpg" , title: "Steak & Shrimp"},
+  {id: 4, image: "https://www.ketodietyum.com/wp-content/uploads/2020/06/Keto-Bunless-Burger-Recipes.jpg" , title: "Wings"},
+  {id: 5, image: "https://www.ketodietyum.com/wp-content/uploads/2020/06/Keto-Bunless-Burger-Recipes.jpg" , title: "Salads"},
+  {id: 6, image: "https://www.ketodietyum.com/wp-content/uploads/2020/06/Keto-Bunless-Burger-Recipes.jpg" , title: "Sides"},
+  {id: 7, image: "https://www.ketodietyum.com/wp-content/uploads/2020/06/Keto-Bunless-Burger-Recipes.jpg" , title: "Charcuterie"},
+  {id: 8, image: "https://www.ketodietyum.com/wp-content/uploads/2020/06/Keto-Bunless-Burger-Recipes.jpg" , title: "Lunches"},
+  {id: 9, image: "https://www.ketodietyum.com/wp-content/uploads/2020/06/Keto-Bunless-Burger-Recipes.jpg" , title: "Kids Menu"},
+  {id: 10, image: "https://www.ketodietyum.com/wp-content/uploads/2020/06/Keto-Bunless-Burger-Recipes.jpg" , title: "Desserts"}
+  
+]
+
+function foodCategories(menuCategories){
+menuCategories.forEach(function(categorie){
+let cards = $(`
+  <div class="card" style="width: 18rem;" >
+    <img class="card-img-top" src="${categorie.image}" alt="Card image cap">
+    <div class="card-body">
+      <p class="card-text">${categorie.title}</p>
+    </div>
+  </div>  
+
+
+`)
+$('.menu-categories').append(cards);
+return cards;
+}
+
+
+}
+
