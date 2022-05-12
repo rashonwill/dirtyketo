5385938f21731d27d6418b0588ccfd11120cd7e1
rashonwill <75400387+rashonwill@users.noreply.github.com>
2022-05-12 15:50:15 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
+let count = 0;
 let cartTotal = 0.00;
-
 (function(){
   let spending = localStorage.getItem('cartCount')
   if (!spending){
-  localStorage.setItem('cartItems', 0)
+  localStorage.setItem('cartCount', count)
   }else if (spending){
 $('.count').text(spending);
   }
@@ -13,9 +13,20 @@ $('.count').text(spending);
 (function(){
   let spending = parseInt(localStorage.getItem('cartCount'))
   if (spending >= 1){
-  $('.modal-body').text('Total Due: $32.76') 
+    let cartItems = JSON.parse(localStorage.getItem('MyItems'))
+    let food = $(`
+       <tr class="myPurchase">
+      <td id="image"><img class="pic" src="${cartItems.image}"></td>
+      <td>${cartItems.name}</td>
+      <td></td>
+      <td class="increase"><input type="text" value="1" class="item-qty"></td>
+      <td class="item-price">${cartItems.price}</td>
+      <td><i class="fa fa-times-circle-o" aria-hidden="true"></i></td>
+    </tr> 
+`).data('cartItems', cartItems)
+  $('.modal-body').append(cartItems) 
   }else if(spending == 0){
   let cartTot = localStorage.setItem('cartTotal', cartTotal)
-  $('.modal-body').text('Your cart is empty') 
+  $('.empty-cart').text('Your cart is empty') 
   }
-})();
+})()
