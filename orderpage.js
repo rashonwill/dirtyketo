fb3122d44b2f6a8129d41dc3f2fb2a342822fb14
rashonwill <75400387+rashonwill@users.noreply.github.com>
2022-05-12 15:50:39 -0500

    Update orderpage.js

--- orderpage.js
+++ orderpage.js
@@ -13,12 +13,23 @@ $('.count').text(spending);
 (function(){
   let spending = parseInt(localStorage.getItem('cartCount'))
   if (spending >= 1){
-  $('.modal-body').text('Total Due: $32.76') 
+    let cartItems = JSON.parse(localStorage.getItem('MyItems'))
+    let food = $(`
+       <tr class="myPurchase">
+      <td id="image"><img class="pic" src="${cartItems.image}"></td>
+      <td>${cartItems.name}</td>
+      <td></td>
+      <td class="increase"><input type="text" value="1" class="item-qty"></td>
+      <td class="item-price">${cartItems.price}</td>
+      <td><i class="fa fa-times-circle-o" aria-hidden="true"></i></td>
+    </tr> 
+`).data('cartItems', cartItems)
+  $('.modal-body').append(cartItems) 
   }else if(spending == 0){
   let cartTot = localStorage.setItem('cartTotal', cartTotal)
-  $('.modal-body').text('Your cart is empty') 
+  $('.empty-cart').text('Your cart is empty') 
   }
-})();
+})()
 
 
 
