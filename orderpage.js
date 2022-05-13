3ef9d7fabd1c4c8ff87152599ed22bc9505523d3
rashonwill <75400387+rashonwill@users.noreply.github.com>
2022-05-13 13:39:49 -0500

    Update orderpage.js

--- orderpage.js
+++ orderpage.js
@@ -99,39 +99,23 @@ myItems = [...myItems, newItem]
 localStorage.setItem('MyItems', JSON.stringify(myItems))
 console.log(myItems);	
 }
-
-updateCart();	
-
-})
-
-//Update Cart
-
-function updateCart(){
-  $('#inthebag tbody').empty()	
-  let items =[]
-  let spending = parseInt(localStorage.getItem('cartCount'))
-  if (spending >= 1){
-    let cartItems = JSON.parse(localStorage.getItem('MyItems'))
-    items.push(cartItems)
-    cartItems.forEach(async function(items){
-        let food = $(`
+	
+   let food = $(`
        <tr class="myPurchase">
        <td></td>
-      <td id="image"><img class="pic" src="${items.image}"></td>
-      <td>${items.name}</td>
+      <td id="image"><img class="pic" src="${newItem.image}"></td>
+      <td>${newItem.name}</td>
       <td></td>
       <td class="increase"><input type="text" value="1" class="item-qty" readonly></td>
       <td></td>
-      <td class="item-price">${items.price}</td>
+      <td class="item-price">${newItem.price}</td>
       <td><i class="fa fa-times-circle-o" aria-hidden="true"></i></td>
       <td></td>
       
     </tr> 
-`).data('cartItems', cartItems)
+`).data('newItem', newItem)
   $('#inthebag tbody').append(food) 
-   })
 	  
-	
 $(food).on('click','.fa-times-circle-o', async function(){	
 let newCart = [];	
 let producttoRemove = $(this).closest('.myPurchase').data('cartItems');
@@ -144,17 +128,10 @@ let removingPurchase = theCart.findIndex(items => items.productid === producttoR
  console.log(newCart)
  $(this).closest('.myPurchase').remove();	
  localStorage.setItem('MyCartItems', JSON.stringify(newCart))
- 			      
-let cartItem = localStorage.getItem('cartCount')
-count = cartItem - 1;
-localStorage.setItem('cartItems', count)	
-$('.count').text(count);
 	
 
-})	  
-	  
+})	
 
-  }
+})
 
 
-}
