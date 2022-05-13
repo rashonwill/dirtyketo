520f3ca59fc9428b72c9b49ba4fcf6efeb8b05f2
rashonwill <75400387+rashonwill@users.noreply.github.com>
2022-05-13 16:05:07 -0500

    Update orderpage.js

--- orderpage.js
+++ orderpage.js
@@ -193,7 +193,7 @@ paypal.Buttons({
     return actions.order.create({
       purchase_units: [{
         amount: {
-          value: localStorage.getItem('cartTotal')
+          value: JSON.parse(localStorage.getItem('cartTotal'))
         }
       }]
     });
