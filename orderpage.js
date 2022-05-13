980caf849173c587fb21c5090c46a34264f3344b
rashonwill <75400387+rashonwill@users.noreply.github.com>
2022-05-13 15:51:42 -0500

    Update orderpage.js

--- orderpage.js
+++ orderpage.js
@@ -187,4 +187,24 @@ getTotal();
      $('.modal-footer .carttotal h1').append('Cart Total: $' + totalCart)	 
     }
 
+paypal.Buttons({
+  createOrder: function(data, actions) {
+    // This function sets up the details of the transaction, including the amount and line item details.
+    return actions.order.create({
+      purchase_units: [{
+        amount: {
+          value: localStorage.getItem('cartTotal')
+        }
+      }]
+    });
+  },
+  onApprove: function(data, actions) {
+    // This function captures the funds from the transaction.
+    return actions.order.capture().then(function(details) {
+      // This function shows a transaction success message to your buyer.
+      alert('Transaction completed by ' + details.payer.name.given_name);
+    });
+  }
+}).render('.paypal');
+
 
